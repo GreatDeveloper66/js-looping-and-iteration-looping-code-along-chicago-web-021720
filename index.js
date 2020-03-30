970bacc1314dbbe8ffa9edc2fe35cc0ebf75df63
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-29 20:16:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+// Code your solutions in this file
+const writeCards = (strArray,message) =>
+  [ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ];
+
+const countDown = () => {
+  let i = 10;
+  while(i >= 0){
+    console.log(i);
+    i--;
+  }
+};
